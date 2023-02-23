011d02fe8cbf5be3ccfa3395394c3e7df3de674e
Saplonily <Saplonily@outlook.com>
2023-02-23 16:05:04 +0800

    增加多图支持

--- SalParticleSys.cs
+++ SalParticleSys.cs
@@ -7,10 +7,12 @@ namespace Saladim.GodotParticle;
 [Tool]
 public partial class SalParticleSys : Node2D
 {
-    protected Random r = new();
+    protected Random r;
     protected List<ParticleUnit> particles;
     protected ObjectPool<ParticleUnit> pool;
     protected float storedAmount;
+
+#if TOOLS
 
     [Export]
     public bool LongShooting { get; set; }
@@ -18,89 +20,109 @@ public partial class SalParticleSys : Node2D
     [Export]
     public float LongShootingShootAmount { get; set; } = 1f;
 
+#endif
+
     [Export]
     public bool LocalCoord { get; set; }
 
     #region Lifetime
 
-    [Export(PropertyHint.Range, "0,8,0.02,or_greater"), ExportGroup("Lifetime")]
-    public float Lifetime { get; set; } = 5f;
+    [Export(PropertyHint.Range, "0,8,0.02,or_greater"), ExportGroup("Lifetime", "Particle")]
+    public float ParticleLifetime { get; set; } = 5f;
 
     [Export(PropertyHint.Range, "0,8,0.02,or_greater")]
-    public float LifetimeRandomness { get; set; } = 0f;
+    public float ParticleLifetimeRandomness { get; set; } = 0f;
 
     #endregion
 
     #region Appearance
 
-    [Export, ExportGroup("Appearance")]
-    public Texture2D Texture { get; set; } = null!;
+    [Export, ExportGroup("Appearance", "Particle")]
+    public Godot.Collections.Array<Texture2D> ParticleTexture { get; set; } = null!;
 
     [Export(PropertyHint.Link)]
-    public Vector2 TextureOrginal { get; set; }
+    public Vector2 ParticleTextureOrginal { get; set; }
+
     [Export]
-    public Gradient Gradient { get; set; } = null!;
+    public Gradient ParticleGradient { get; set; } = null!;
+
+    #endregion
+
+    #region Animation
+
+    [Export(PropertyHint.Range, "0,1,0.01,or_greater"), ExportGroup("Animation", "Particle")]
+    public float ParticleAnimationSpeed { get; set; }
+
+    [Export(PropertyHint.Range, "0,1,0.01,or_greater"), ExportGroup("Animation", "Particle")]
+    public float ParticleAnimationSpeedRandomness { get; set; }
 
     #endregion
 
     #region Position and Speed
 
-    [Export, ExportGroup("Position And Speed")]
-    public Vector2 InitPosition { get; set; }
+    [Export, ExportGroup("Position And Speed", "Particle")]
+    public Vector2 ParticlePosition { get; set; }
 
     [Export]
-    public Vector2 InitPositionRandomness { get; set; }
+    public Vector2 ParticlePositionRandomness { get; set; }
 
     [Export]
-    public Vector2 InitSpeed { get; set; }
+    public Vector2 ParticleSpeed { get; set; }
 
     [Export]
-    public Vector2 InitSpeedRandomness { get; set; }
+    public Vector2 ParticleSpeedRandomness { get; set; }
 
     [Export]
-    public Vector2 Accelerate { get; set; }
+    public Vector2 ParticleAccelerate { get; set; }
 
     [Export]
-    public Vector2 Gravity { get; set; } = Vector2.Down * 100f;
+    public Vector2 ParticleGravity { get; set; } = Vector2.Down * 100f;
 
     #endregion
 
     #region Rotation
 
-    [Export(PropertyHint.Range, "-3.1415926,3.1415926,0.01"), ExportGroup("Rotation")]
-    public float InitRotation { get; set; }
+    [Export(PropertyHint.Range, "-3.1415926,3.1415926,0.01"), ExportGroup("Rotation", "Particle")]
+    public float ParticleRotation { get; set; } = 0f;
 
     [Export(PropertyHint.Range, "-3.1415926,3.1415926,0.01")]
-    public float InitRotationRandomness { get; set; }
+    public float ParticleRotationRandomness { get; set; } = 0f;
 
     [Export(PropertyHint.Range, "-0.1,0.1,0.0001,or_greater,or_less")]
-    public float InitRotationSpeed { get; set; }
+    public float ParticleRotationSpeed { get; set; } = 0f;
 
     [Export(PropertyHint.Range, "0,0.1,0.0001,or_greater")]
-    public float InitRotationSpeedRandomness { get; set; }
+    public float ParticleRotationSpeedRandomness { get; set; } = 0f;
 
     #endregion
 
-    public SalParticleSys()
+    public SalParticleSys(Random r)
     {
         var policy = new ParticleUnit.PooledObjectPolicy();
         pool = new DefaultObjectPool<ParticleUnit>(policy);
         particles = new(32);
+        this.r = r;
+    }
+
+    public SalParticleSys() : this(new())
+    {
     }
 
     public ParticleUnit Emit()
     {
         var u = pool.Get();
 
-        u.MaxLifeTime = Lifetime;
-        u.LifeTime = FloatRandomize(Lifetime, LifetimeRandomness, r);
+        u.MaxLifeTime = ParticleLifetime;
+        u.LifeTime = FloatRandomize(ParticleLifetime, ParticleLifetimeRandomness, r);
 
-        u.Position = VectorRandomize(InitPosition, InitPositionRandomness, r);
+        u.Position = VectorRandomize(ParticlePosition, ParticlePositionRandomness, r);
 
-        u.Speed = VectorRandomize(InitSpeed, InitSpeedRandomness, r);
+        u.Speed = VectorRandomize(ParticleSpeed, ParticleSpeedRandomness, r);
 
-        u.Rotation = FloatRandomize(InitRotation, InitRotationRandomness, r);
-        u.RotationSpeed = FloatRandomize(InitRotationSpeed, InitRotationSpeedRandomness, r);
+        u.Rotation = FloatRandomize(ParticleRotation, ParticleRotationRandomness, r);
+        u.RotationSpeed = FloatRandomize(ParticleRotationSpeed, ParticleRotationSpeedRandomness, r);
+
+        u.AnimationSpeed = FloatRandomize(ParticleAnimationSpeed, ParticleAnimationSpeedRandomness, r);
 
         if (!LocalCoord)
         {
@@ -130,13 +152,14 @@ public partial class SalParticleSys : Node2D
 
     public override void _Ready()
     {
-        Gradient ??= new Gradient();
+        ParticleGradient ??= new Gradient();
     }
 
     public override void _Process(double delta)
     {
         QueueRedraw();
 
+#if TOOLS
         if (LongShooting)
         {
             storedAmount += LongShootingShootAmount;
@@ -146,16 +169,22 @@ public partial class SalParticleSys : Node2D
                 Emit();
             }
         }
+#endif
 
         for (int i = particles.Count - 1; i >= 0; i--)
         {
             var p = particles[i];
-            p.Speed += Gravity * (float)delta;
-            p.Speed += Accelerate * (float)delta;
+            p.Speed += ParticleGravity * (float)delta;
+            p.Speed += ParticleAccelerate * (float)delta;
             p.Position += p.Speed * (float)delta;
             p.LifeTime -= (float)delta;
-            p.Color = Gradient.Sample(1f - p.LifeTime / p.MaxLifeTime);
+            p.Color = ParticleGradient.Sample(1f - p.LifeTime / p.MaxLifeTime);
             p.Rotation += p.RotationSpeed;
+            p.AnimationProcess += p.AnimationSpeed;
+            if ((int)p.AnimationProcess > ParticleTexture.Count - 1)
+                p.AnimationProcess = 0f;
+            if (p.AnimationProcess < 0f) 
+                p.AnimationProcess = 0f;
             if (p.LifeTime <= 0)
             {
                 pool.Return(p);
@@ -169,10 +198,10 @@ public partial class SalParticleSys : Node2D
 #if TOOLS
         if (Engine.IsEditorHint())
         {
-            if (Texture is null) return;
-            DrawCircle(InitPosition, 2, Color.Color8(100, 100, 255, 50));
+            if (ParticleTexture is null) return;
+            DrawCircle(ParticlePosition, 2, Color.Color8(100, 100, 255, 50));
             DrawRect(
-                new Rect2(InitPosition - InitPositionRandomness, InitPositionRandomness * 2),
+                new Rect2(ParticlePosition - ParticlePositionRandomness, ParticlePositionRandomness * 2),
                 Color.Color8(100, 100, 255, 50),
                 false,
                 2
@@ -181,12 +210,13 @@ public partial class SalParticleSys : Node2D
 #endif
         foreach (var p in particles)
         {
-            var size = Texture.GetSize();
+            int process = (int)p.AnimationProcess;
+            var size = ParticleTexture[process].GetSize();
             Transform2D trans = Transform2D.Identity;
             if (!LocalCoord) trans *= Transform.AffineInverse();
             trans *= new Transform2D(p.Rotation, Vector2.One, 0, p.Position);
             DrawSetTransformMatrix(trans);
-            DrawTexture(Texture, TextureOrginal * size, p.Color);
+            DrawTexture(ParticleTexture[process], ParticleTextureOrginal * size, p.Color);
         }
     }
 
@@ -206,12 +236,13 @@ public partial class SalParticleSys : Node2D
 
         public float RotationSpeed { get; set; }
 
+        public float AnimationSpeed { get; set; }
+
+        public float AnimationProcess { get; set; }
+
         public class PooledObjectPolicy : PooledObjectPolicy<ParticleUnit>
         {
-            public override ParticleUnit Create()
-            {
-                return new();
-            }
+            public override ParticleUnit Create() => new();
 
             public override bool Return(ParticleUnit p)
             {
@@ -222,6 +253,7 @@ public partial class SalParticleSys : Node2D
                 p.Rotation = default;
                 p.Color = new Color(1, 1, 1, 1);
                 p.RotationSpeed = default;
+                p.AnimationProcess = 0;
                 return true;
             }
         }
